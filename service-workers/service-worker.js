60a87cb6397d19d795f05a374a0420062e4f0c03
cristiano070 <cristiano070@hotmail.com>
2023-02-22 13:05:21 -0300

    Update service-worker.js

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,1 +1,8 @@
 // https://cristiano070.github.io/service-workers/
+
+self.addEventListener('install', function (e) {
+    console.log('Install Event:', e);
+});
+self.addEventListener('activate', function (e) {
+    console.log('Activate Event:', e);
+});
